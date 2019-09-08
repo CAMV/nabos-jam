8d03ffbafb56f70bccddab5eb16025920ab2d008
Gabriel Gedler <gabrielgedler@gmail.com>
2019-09-08 14:40:59 -0400

    Added resource object and added health as child

---
+++ nabos-jam/Assets/Scripts/ScriptableObjects/UnitResources/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character/Health")]
+public class Health : Resource
+{
+    [SerializeField]
+    public int maxHealth {
+        get {
+            return maxResource;
+        }
+        set {
+            maxHealth = value;
+        }
+    }
+    [SerializeField]
+    public int currentHealth {
+        get {
+            return currentResource;
+        }
+        set {
+            currentHealth = value;
+        }
+    }
+
+    public Health(int maxHealth) 
+    {
+        this.maxHealth = maxHealth;
+        currentHealth = maxHealth;
+    }
+
+    public void modifyHealth(int healthMod)
+    {
+        modifyResource(healthMod);  
+    }
+}
