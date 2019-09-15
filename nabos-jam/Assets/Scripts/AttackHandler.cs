9220b8547aa1fa3318425cf5fc4b18ad1c9547ac
Gabriel Gedler <gabrielgedler@gmail.com>
2019-09-14 22:34:14 -0400

    moved attack action for further refactoring

--- nabos-jam/Assets/Scripts/AttackHandler.cs
+++ nabos-jam/Assets/Scripts/AttackHandler.cs
@@ -9,11 +9,57 @@ public class AttackHandler : MonoBehaviour
 {
     public Character character;
     public bool isAttacking = false;
+    private bool _isChasing = false;
     
     private void FixedUpdate() 
     {
         //Reduce character cooldowns
         float tick = Time.fixedDeltaTime;
         character.TickDurations(tick);
+    }
+
+    public void Attack(Unit _enemyUnit)
+    {
+        Unit _myUnit = GetComponent<Unit>();
+        float attackCd = character.attackCooldown;
+        //Checks if unit hasn't died
+        if (_enemyUnit != null) 
+        {
+            if (character.attackRange > Vector3.Distance(transform.position, _enemyUnit.transform.position))
+            {
+                //Stops moving once withing range
+                if (_isChasing && _myUnit.Movement)
+                {
+                    _isChasing = false;
+                    _myUnit.Movement.StopMoving();
+                }
+                //Checks if attack cooldown is ready
+                if (attackCd <= 0)
+                {
+                    _myUnit.Character.PerformAttack(_enemyUnit.Character);
+                    //Reset attack delay
+                    _myUnit.Character.ResetAttackCd();
+
+                    //Unit died, destroy it
+                    if (_enemyUnit.Character.health.currentHealth <= 0) 
+                    {
+                        Object.Destroy(_enemyUnit.gameObject);
+                        _myUnit.GetComponent<AttackHandler>().isAttacking = false;
+                    }
+                }
+
+            }
+            else 
+            {
+                if (_myUnit.Movement && !_isChasing)
+                    _isChasing = true;
+                    _myUnit.Movement.Move(_enemyUnit.transform.position);
+            }
+
+        }
+        if (_myUnit.GetComponent<AttackHandler>().isAttacking)
+        {
+            GameManager.Instance.PlayerSquad.AddCommand(new AttackCmd(_myUnit, _enemyUnit));
+        }
     }
 }
--- nabos-jam/Assets/Scripts/Commands/AttackCmd.cs
+++ nabos-jam/Assets/Scripts/Commands/AttackCmd.cs
@@ -6,7 +6,6 @@ public class AttackCmd : Command
 {
     private Unit _myUnit;
     private Unit _enemyUnit;
-    private bool _isChasing = false;
     
 
     public AttackCmd(Unit unit, Unit enemyUnit)
@@ -15,56 +14,10 @@ public class AttackCmd : Command
         this._enemyUnit = enemyUnit;
     }
 
-    public AttackCmd(Unit unit, Unit enemyUnit, bool isChasing)
-    {
-        this._myUnit = unit;
-        this._enemyUnit = enemyUnit;
-        _isChasing = isChasing;
-    }
-
 
     override public void Do()
     {
-        float attackCd = _myUnit.Character.attackCooldown;
-        //Checks if unit hasn't died
-        if (_enemyUnit != null) 
-        {
-            if (_myUnit.Character.attackRange > Vector3.Distance(_myUnit.transform.position, _enemyUnit.transform.position))
-            {
-                //Stops moving once withing range
-                if (_isChasing && _myUnit.Movement)
-                {
-                    _isChasing = false;
-                    _myUnit.Movement.StopMoving();
-                }
-                //Checks if attack cooldown is ready
-                if (attackCd <= 0)
-                {
-                    _myUnit.Character.PerformAttack(_enemyUnit.Character);
-                    //Reset attack delay
-                    _myUnit.Character.ResetAttackCd();
-
-                    //Unit died, destroy it
-                    if (_enemyUnit.Character.health.currentHealth <= 0) 
-                    {
-                        Object.Destroy(_enemyUnit.gameObject);
-                        _myUnit.GetComponent<AttackHandler>().isAttacking = false;
-                    }
-                }
-
-            }
-            else 
-            {
-                if (_myUnit.Movement && !_isChasing)
-                    _isChasing = true;
-                    _myUnit.Movement.Move(_enemyUnit.transform.position);
-            }
-
-        }
-        if (_myUnit.GetComponent<AttackHandler>().isAttacking)
-        {
-            Requeue();
-        }
+        _myUnit.GetComponent<AttackHandler>().Attack(_enemyUnit);
     }
 
     /// <summary>
@@ -72,7 +25,6 @@ public class AttackCmd : Command
     /// </summary>
     public void Requeue()
     {
-        GameManager.Instance.PlayerSquad.AddCommand(new AttackCmd(_myUnit, _enemyUnit, _isChasing));
     }
 
     override public void Undo()
